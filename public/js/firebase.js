1e67df5f1eb4be6b22348bd8858d6eccb5b97c96
Oscar Galindo <funcionmain@gmail.com>
2026-03-13 12:34:18 +0000

    integracionSdkFirebase

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,14 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-
-apiKey: "TU_API_KEY",
-authDomain: "TU_PROYECTO.firebaseapp.com",
-projectId: "TU_PROYECTO",
-storageBucket: "TU_PROYECTO.appspot.com"
-
+  apiKey: "AIzaSyDuMC1oxifODI9wt4vnZRarwFBgo66sXkE",
+  authDomain: "tienda-ding-dong.firebaseapp.com",
+  projectId: "tienda-ding-dong",
+  storageBucket: "tienda-ding-dong.firebasestorage.app",
+  messagingSenderId: "379208026977",
+  appId: "1:379208026977:web:3e74844ee1e92482489320",
+  measurementId: "G-Z60MYHFEY3"
 };
 
-firebase.initializeApp(firebaseConfig);
-
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const auth = firebase.auth();
 const db = firebase.firestore();
 const storage = firebase.storage();

1544c7d8529bfad5d33a2ba443a683fef44f1264
Oscar Galindo <funcionmain@gmail.com>
2026-03-25 12:48:03 +0000

    panelesCMR

--- public/js/firebase-config.js
+++ public/js/firebase-config.js
@@ -1,18 +1,11 @@
-
-
 const firebaseConfig = {
-  apiKey: "AIzaSyDuMC1oxifODI9wt4vnZRarwFBgo66sXkE",
-  authDomain: "tienda-ding-dong.firebaseapp.com",
-  projectId: "tienda-ding-dong",
-  storageBucket: "tienda-ding-dong.firebasestorage.app",
-  messagingSenderId: "379208026977",
-  appId: "1:379208026977:web:3e74844ee1e92482489320",
-  measurementId: "G-Z60MYHFEY3"
+  apiKey: "REEMPLAZA_API_KEY",
+  authDomain: "REEMPLAZA_AUTH_DOMAIN",
+  projectId: "REEMPLAZA_PROJECT_ID",
+  storageBucket: "REEMPLAZA_STORAGE_BUCKET",
+  messagingSenderId: "REEMPLAZA_MESSAGING_SENDER_ID",
+  appId: "REEMPLAZA_APP_ID"
 };
-
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
-
-window.auth = firebase.auth();
-window.db = firebase.firestore();
+if (!firebase.apps.length) firebase.initializeApp(firebaseConfig);
+window.auth = firebase.auth ? firebase.auth() : null;
+window.db = firebase.firestore ? firebase.firestore() : null;

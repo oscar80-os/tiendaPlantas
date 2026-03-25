5d612780a76e70ac39d8e45fc5f96be65f3a16e0
Oscar Galindo <funcionmain@gmail.com>
2026-03-25 14:59:08 +0000

    botonCursos1

--- public/js/firebase-config.js
+++ public/js/firebase-config.js
@@ -1,11 +1,13 @@
 const firebaseConfig = {
-  apiKey: "REEMPLAZA_API_KEY",
-  authDomain: "REEMPLAZA_AUTH_DOMAIN",
-  projectId: "REEMPLAZA_PROJECT_ID",
-  storageBucket: "REEMPLAZA_STORAGE_BUCKET",
-  messagingSenderId: "REEMPLAZA_MESSAGING_SENDER_ID",
-  appId: "REEMPLAZA_APP_ID"
+  apiKey: "AIzaSyDuMC1oxifODI9wt4vnZRarwFBgo66sXkE",
+  authDomain: "tienda-ding-dong.firebaseapp.com",
+  projectId: "tienda-ding-dong",
+  storageBucket: "tienda-ding-dong.firebasestorage.app",
+  messagingSenderId: "379208026977",
+  appId: "1:379208026977:web:3e74844ee1e92482489320",
+  
 };
 if (!firebase.apps.length) firebase.initializeApp(firebaseConfig);
 window.auth = firebase.auth ? firebase.auth() : null;
 window.db = firebase.firestore ? firebase.firestore() : null;
+
